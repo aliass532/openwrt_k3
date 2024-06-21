7827cb1d086ba641818306269d616ba3cda04252
aliass532 <56620619+aliass532@users.noreply.github.com>
2024-06-21 23:23:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i 's/192.168.1.1/192.168.11.1/g' package/base-files/files/bin/config_generate
 sed -i '3a iwconfig wlan0 txpower 24' package/base-files/files/etc/rc.local
 sed -i '4a iwconfig wlan1 txpower 24' package/base-files/files/etc/rc.local
